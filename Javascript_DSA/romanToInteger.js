642ed740d091f1c9bbbbd94ea39c0fdc66e9ea8c
jugosack <jugoslav_ackoski@yahoo.com>
2023-09-20 12:56:39 +0200

    Add content to code

--- Javascript_DSA/romanToInteger.js
+++ Javascript_DSA/romanToInteger.js
@@ -1,13 +1,44 @@
 /**
- * @param {string} s
- * @return {number}
- */
- var romanToInt = function(s) {
-    const Map ={I:1, V:5, X:10, L:50, C:100, D:500, M:1000}
-    
+* @param {string} s
+* @return {number}
+*/
+var romanToInt = function (s) {
+ const Map = { I: 1, V: 5, X: 10, L: 50, C: 100, D: 500, M: 1000 };
+ let res = 0;
+ s.split('').forEach((num, i) => {
+   if (Map[num] < Map[s[i + 1]]) res -= Map[num];
+   else res += Map[num];
+ });
+ return res;
 };
+
 // Test cases
-console.log(romanToInt("III"));        // Output: 3
-console.log(romanToInt("V"));        // Output: 5
-console.log(romanToInt("X"));        // Output: 10
-console.log(romanToInt("L"));        // Output: 50
+console.log(romanToInt('III')); // Output: 3
+console.log(romanToInt('V')); // Output: 5
+console.log(romanToInt('X')); // Output: 10
+console.log(romanToInt('L')); // Output: 50
+
+// /**
+//  * @param {string} s
+//  * @return {number}
+//  */
+//  var romanToInt = function (s) {
+//     const Map = { I: 1, V: 5, X: 10, L: 50, C: 100, D: 500, M: 1000 };
+//     let res = 0;
+  
+//     for (let i = 0; i < s.length; i++) {
+//       if (Map[s[i]] < Map[s[i + 1]]) {
+//         res -= Map[s[i]];
+//       } else {
+//         res += Map[s[i]];
+//       }
+//     }
+  
+//     return res;
+//   };
+  
+//   // Test cases
+//   console.log(romanToInt('III')); // Output: 3
+//   console.log(romanToInt('V')); // Output: 5
+//   console.log(romanToInt('X')); // Output: 10
+//   console.log(romanToInt('L')); // Output: 50
