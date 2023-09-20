18b8a330299fc799b4aff4e80c6479a4e59f8356
jugosack <jugoslav_ackoski@yahoo.com>
2023-09-20 12:56:58 +0200

    Add content to code

--- Javascript_DSA/romanToInteger.js
+++ Javascript_DSA/romanToInteger.js
@@ -17,28 +17,3 @@ console.log(romanToInt('III')); // Output: 3
 console.log(romanToInt('V')); // Output: 5
 console.log(romanToInt('X')); // Output: 10
 console.log(romanToInt('L')); // Output: 50
-
-// /**
-//  * @param {string} s
-//  * @return {number}
-//  */
-//  var romanToInt = function (s) {
-//     const Map = { I: 1, V: 5, X: 10, L: 50, C: 100, D: 500, M: 1000 };
-//     let res = 0;
-  
-//     for (let i = 0; i < s.length; i++) {
-//       if (Map[s[i]] < Map[s[i + 1]]) {
-//         res -= Map[s[i]];
-//       } else {
-//         res += Map[s[i]];
-//       }
-//     }
-  
-//     return res;
-//   };
-  
-//   // Test cases
-//   console.log(romanToInt('III')); // Output: 3
-//   console.log(romanToInt('V')); // Output: 5
-//   console.log(romanToInt('X')); // Output: 10
-//   console.log(romanToInt('L')); // Output: 50
