fb8b170427611558f3a492b17d47dc7d529ee3a3
jugosack <jugoslav_ackoski@yahoo.com>
2023-09-20 12:05:44 +0200

    Add romanToIntegr.js

---
+++ Javascript_DSA/romanToInteger.js
@@ -0,0 +1,11 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+ var romanToInt = function(s) {
+    
+};
+// Test cases
+console.log(romanToInt("III"));        // Output: 3
+console.log(romanToInt("V"));        // Output: 5
+console.log(romanToInt("x"));        // Output: 10
