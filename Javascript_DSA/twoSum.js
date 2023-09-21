7227b7d1fd9644010f56a6e9d924d46108864f8f
jugosack <jugoslav_ackoski@yahoo.com>
2023-09-21 15:20:36 +0200

    Add twoSum.js test casses

--- Javascript_DSA/twoSum.js
+++ Javascript_DSA/twoSum.js
@@ -6,3 +6,9 @@
  var twoSum = function(nums, target) {
     
 };
+
+console.log(twoSum([2, 7, 11, 15], 9)); // Expected output: [0, 1]
+console.log(twoSum([3, 2, 4], 6));      // Expected output: [1, 2]
+console.log(twoSum([3, 3], 6));          // Expected output: [0, 1]
+console.log(twoSum([-1, -2, -3, -4, -5], -8)); // Expected output: [2, 4]
+console.log(twoSum([1, 2, 3, 4], 10));    // Expected output: []
