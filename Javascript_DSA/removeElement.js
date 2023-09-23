afc3a88541be616d7d0f6f5e78076d5ed078f419
jugosack <jugoslav_ackoski@yahoo.com>
2023-09-23 22:00:35 +0200

    Add removeElement,js test cases

---
+++ Javascript_DSA/removeElement.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+ var removeElement = function(nums, val) {
+    
+};
+
+// Test cases
+console.log(removeElement([2, 7, 11, 15], 7)); // Expected output: [2,11,15] = nums, k=3
+console.log(removeElement([3, 2, 4, 6],6));      
+console.log(removeElement([3, 3, 4], 4));          
+
