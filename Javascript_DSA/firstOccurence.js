0c2398b54606dcd5a9200543fa4a496412ede5fb
jugosack <jugoslav_ackoski@yahoo.com>
2023-09-24 22:47:58 +0200

    Add firstOccurence.js test cases

--- Javascript_DSA/firstOccurence.js
+++ Javascript_DSA/firstOccurence.js
@@ -12,8 +12,6 @@ console.log(strStr("hello", "ll")); // Expected output: 2
 console.log(strStr("aaaaa", "bba")); // Expected output: -1 (not found)
 console.log(strStr("", "a")); // Expected output: -1 (empty haystack)
 console.log(strStr("a", "")); // Expected output: 0 (empty needle)
-console.log(strStr("abcdef", "abc")); // Expected output: 0 (found at the beginning)
-console.log(strStr("abcdef", "efg")); // Expected output: 4 (found towards the end)
 console.log(strStr("abcdef", "xyz")); // Expected output: -1 (not found)
 console.log(strStr("aaaaaa", "a")); // Expected output: 0 (found at the beginning)
 console.log(strStr("mississippi", "issip")); // Expected output: 4
