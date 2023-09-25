d3d16e31bc6200783c13db5518b79c8f4da358f5
jugosack <jugoslav_ackoski@yahoo.com>
2023-09-25 21:33:21 +0200

    removeDuplicateElement.js test cases

--- Javascript_DSA/removeDuplicateElement.js
+++ Javascript_DSA/removeDuplicateElement.js
@@ -5,3 +5,28 @@
  var removeDuplicates = function(nums) {
    
 };
+//Test cases
+// Test case 1: Basic test case with duplicates
+const nums1 = [1, 1, 2];
+console.log(removeDuplicates(nums1)); // Expected output: 2
+// After removing duplicates, nums1 should be [1, 2]
+
+// Test case 2: Array with no duplicates
+const nums2 = [1, 2, 3];
+console.log(removeDuplicates(nums2)); // Expected output: 3
+// Since there are no duplicates, the length remains the same
+
+// Test case 3: Array with a single element
+const nums3 = [1];
+console.log(removeDuplicates(nums3)); // Expected output: 1
+// There's only one element, so no duplicates to remove
+
+// Test case 4: Empty array
+const nums4 = [];
+console.log(removeDuplicates(nums4)); // Expected output: 0
+// The array is empty, so there are no duplicates
+
+// Test case 5: Array with all elements as duplicates
+const nums5 = [2, 2, 2, 2, 2];
+console.log(removeDuplicates(nums5)); // Expected output: 1
+// After removing duplicates, nums5 should be [2]
