bdb4d49eb0826ac191d6484875156f4f6265041d
jugosack <jugoslav_ackoski@yahoo.com>
2023-09-25 21:46:06 +0200

    removeDuplicateElement.js solution

--- Javascript_DSA/removeDuplicateElement.js
+++ Javascript_DSA/removeDuplicateElement.js
@@ -1,39 +1,27 @@
 /**
- * @param {number[]} nums
- * @return {number}
- */
- var removeDuplicates = function(nums) {
-    if (nums.length === 0) {
-        return 0; // If the array is empty, there are no duplicates.
-    }
+* @param {number[]} nums
+* @return {number}
+*/
+var removeDuplicates = function(nums) {
+   if (nums.length === 0) {
+       return 0; // If the array is empty, there are no duplicates.
+   }
 
-    if (nums.length === 0) {
-        return 0; // If the array is empty, there are no duplicates.
-    }
+   let uniqueIndex = 0; // Index for the unique elements.
 
+   for (let i = 1; i < nums.length; i++) {
+       if (nums[i] !== nums[uniqueIndex]) {
+           uniqueIndex++;
+           nums[uniqueIndex] = nums[i];
+       }
+   }
+
+   // The unique elements are now in the subarray nums[0] to nums[uniqueIndex].
+   // The length of the array with duplicates removed is uniqueIndex + 1.
+   return uniqueIndex + 1;
 };
+
 //Test cases
-// Test case 1: Basic test case with duplicates
-const nums1 = [1, 1, 2];
-console.log(removeDuplicates(nums1)); // Expected output: 2
-// After removing duplicates, nums1 should be [1, 2]
+console.log(removeDuplicates([0,0,1,1,1,2,2,3,3,4])); // Expected output: 1
 
-// Test case 2: Array with no duplicates
-const nums2 = [1, 2, 3];
-console.log(removeDuplicates(nums2)); // Expected output: 3
-// Since there are no duplicates, the length remains the same
 
-// Test case 3: Array with a single element
-const nums3 = [1];
-console.log(removeDuplicates(nums3)); // Expected output: 1
-// There's only one element, so no duplicates to remove
-
-// Test case 4: Empty array
-const nums4 = [];
-console.log(removeDuplicates(nums4)); // Expected output: 0
-// The array is empty, so there are no duplicates
-
-// Test case 5: Array with all elements as duplicates
-const nums5 = [2, 2, 2, 2, 2];
-console.log(removeDuplicates(nums5)); // Expected output: 1
-// After removing duplicates, nums5 should be [2]
