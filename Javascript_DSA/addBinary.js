67f2b9fcb2d26e5146734e607d1f1a7e1e6181b0
jugosack <jugoslav_ackoski@yahoo.com>
2023-10-02 10:14:16 +0200

    Add addBinary.js

--- Javascript_DSA/addBinary.js
+++ Javascript_DSA/addBinary.js
@@ -1,11 +1,32 @@
-/**
- * @param {string} a
- * @param {string} b
- * @return {string}
- */
- var addBinary = function(a, b) {
+function addBinary(a, b) {
+    let result = '';
+    let carry = 0;
     
-};
+    // Start from the rightmost digit of both binary strings
+    let i = a.length - 1;
+    let j = b.length - 1;
+    
+    while (i >= 0 || j >= 0 || carry > 0) {
+        const digitA = i >= 0 ? parseInt(a[i]) : 0;
+        const digitB = j >= 0 ? parseInt(b[j]) : 0;
+        
+        // Calculate the sum of current digits and carry
+        const sum = digitA + digitB + carry;
+        
+        // Append the least significant bit of the sum to the result
+        result = (sum % 2) + result;
+        
+        // Update the carry for the next iteration
+        carry = Math.floor(sum / 2);
+        
+        // Move to the next digits
+        i--;
+        j--;
+    }
+    
+    return result;
+}
+
 //Test cases
 console.log(addBinary("11", "1")); // Expected output: 100
 console.log(addBinary("1010", "1011")); // Expected output: 10101
