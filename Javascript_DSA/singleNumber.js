e43704b49ae79b82bae0eadf6db6270f7d1763df
jugosack <jugoslav_ackoski@yahoo.com>
2023-10-03 10:06:27 +0200

    Add singleNumber.js test cases

--- Javascript_DSA/singleNumber.js
+++ Javascript_DSA/singleNumber.js
@@ -3,7 +3,11 @@
  * @return {number}
  */
  var singleNumber = function(nums) {
-   
+    
+    
 };
-
+   
 //Test cases
+console.log(singleNumber([2, 2, 1])); // Output: 1
+console.log(singleNumber([4, 1, 2, 1, 2])); // Output: 4
+console.log(singleNumber([1])); // Output: 1
