65d5241f2979d91e465bc2df48f1377232ee474e
jugosack <jugoslav_ackoski@yahoo.com>
2023-10-05 09:46:53 +0200

    Add containsDuplicate.js Test cases

--- Javascript_DSA/containsDuplicate.js
+++ Javascript_DSA/containsDuplicate.js
@@ -5,5 +5,8 @@
  var containsDuplicate = function(nums) {
    
 };
-//Test cases
-
+// Test cases
+console.log(containsDuplicate([1, 2, 3, 4])); // Should print false (no duplicates)
+console.log(containsDuplicate([1, 2, 2, 4])); // Should print true (2 is duplicated)
+console.log(containsDuplicate([])); // Should print false (empty array)
+console.log(containsDuplicate([1, 1])); // Should print true (1 is duplicated)
