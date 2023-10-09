3c234e3e521da5870d61e928a53fc0fc47ad936e
jugosack <jugoslav_ackoski@yahoo.com>
2023-10-09 09:46:31 +0200

    Add applieandorange.js test cases

--- Javascript_DSA/appleandorange.js
+++ Javascript_DSA/appleandorange.js
@@ -26,3 +26,17 @@ function countApplesAndOranges(s, t, a, b, apples, oranges) {
 }
 
 // Test cases
+// Test Case 1: Apples and oranges both fall within the range [s, t]
+console.log(countApplesAndOranges(5, 10, 5, 7, [1, 3, 7], [6, 8])); // Output: [3, 2]
+
+// Test Case 2: No apples or oranges fall within the range [s, t]
+console.log(countApplesAndOranges(5, 10, 5, 7, [2, 4, 11], [3, 9])); // Output: [0, 0]
+
+// Test Case 3: Only apples fall within the range [s, t]
+console.log(countApplesAndOranges(5, 10, 5, 7, [6, 7, 8], [1, 2, 3])); // Output: [3, 0]
+
+// Test Case 4: Only oranges fall within the range [s, t]
+console.log(countApplesAndOranges(5, 10, 5, 7, [1, 2, 3], [6, 7, 8])); // Output: [0, 3]
+
+// Test Case 5: Apples and oranges fall exactly on the lower and upper bounds of the range [s, t]
+console.log(countApplesAndOranges(5, 10, 5, 7, [5, 10], [5, 10])); // Output: [2, 2]
