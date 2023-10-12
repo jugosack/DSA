795596b3ef429e27f2150cf6c4967c7000a416b4
jugosack <jugoslav_ackoski@yahoo.com>
2023-10-12 21:58:45 +0200

    Add pickingNumbers.js solution

--- Javascript_DSA/pickNumbers.js
+++ Javascript_DSA/pickNumbers.js
@@ -1,4 +1,21 @@
 function pickingNumbers(a) {
-
+    a.sort((a, b) => a - b); // Sort the input array in ascending order.
+    
+    let maxCount = 0;
+    let currentCount = 0;
+    let currentElement = a[0];
+    
+    for (let i = 0; i < a.length; i++) {
+        if (Math.abs(a[i] - currentElement) <= 1) {
+            currentCount++;
+        } else {
+            currentElement = a[i];
+            currentCount = 1;
+        }
+        maxCount = Math.max(maxCount, currentCount);
+    }
+    
+    return maxCount;
 }
+
 //Test cases
