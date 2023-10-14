79f7fb5815e6f32174687f1d4c15833d273322c4
jugosack <jugoslav_ackoski@yahoo.com>
2023-10-14 23:34:54 +0200

    Add miniMaxSum.js test cases

--- Javascript_DSA/miniMaxSum.js
+++ Javascript_DSA/miniMaxSum.js
@@ -4,3 +4,7 @@ function miniMaxSum(arr) {
 }
 
 //Test cases
+const arr=[1,3,5,7,9]
+console.log(miniMaxSum(arr));        // Output: 5
+const arr1=[1,2,3,4,5]
+console.log(miniMaxSum(arr1));        // Output: 3
