d1a6cde375da84a1aefc9b459d34c7e28095cc8d
jugosack <jugoslav_ackoski@yahoo.com>
2023-10-15 20:41:15 +0200

    Add minMaxSum.js variables

--- Javascript_DSA/miniMaxSum.js
+++ Javascript_DSA/miniMaxSum.js
@@ -1,10 +1,12 @@
 function miniMaxSum(arr) {
-    // Write your code here
+   
+var sum1=0
+var sum2=0
 
 }
 
 //Test cases
 const arr=[1,3,5,7,9]
-console.log(miniMaxSum(arr));        // Output: 5
+console.log(miniMaxSum(arr));        // Output: 16 21
 const arr1=[1,2,3,4,5]
-console.log(miniMaxSum(arr1));        // Output: 3
+console.log(miniMaxSum(arr1));        // Output: 10 12
