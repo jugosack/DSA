8da55f9afea0ab9f37dd4784fa3b9c4390695e86
jugosack <jugoslav_ackoski@yahoo.com>
2023-10-16 23:21:51 +0200

    Add numberLineJumps.js solution

--- Javascript_DSA/numberLineJumps.js
+++ Javascript_DSA/numberLineJumps.js
@@ -1,7 +1,23 @@
 function kangaroo(x1, v1, x2, v2) {
     // Write your code here
+    if (v1 == v2) {
+        if (x1 == x2) {
+            return 'YES'; // Kangaroos start at the same position and have the same velocity.
+        } else {
+            return 'NO'; // Kangaroos have the same velocity but different starting positions.
+        }
+    }
 
+    // The following formula checks if the kangaroos will ever land on the same position.
+    // We subtract x2 from x1 and check if the result is divisible evenly by the difference in velocities.
+    // If yes, they will meet at the same position.
+    if ((x2 - x1) % (v1 - v2) === 0 && (x2 - x1) / (v1 - v2) >= 0) {
+        return 'YES';
+    } else {
+        return 'NO';
+    }
 }
+
 //Test cases
 console.log(kangaroo(0, 3, 4, 2)); // Output: 'YES'
 console.log(kangaroo(0, 2, 5, 3)); // Output: 'NO'
