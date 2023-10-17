016ad95d7fd68b18529cf44fb8e954d418328290
jugosack <jugoslav_ackoski@yahoo.com>
2023-10-17 15:07:06 +0200

    Add regexVar.js solution

--- Javascript_DSA/regexVar.js
+++ Javascript_DSA/regexVar.js
@@ -4,7 +4,7 @@ function regexVar() {
      * It must match a string that starts and ends with the same vowel (i.e., {a, e, i, o, u})
      */
    
-
+    var re = /^(a|e|i|o|u).*\1$/i;
     /*
      * Do not remove the return statement
      */
