0b39abce69e0ee697e56381b3ed2e11c53e2a1b4
jugosack <jugoslav_ackoski@yahoo.com>
2023-10-18 21:45:25 +0200

    Add getGrade.js test cases

--- Javascript_DSA/getGrade.js
+++ Javascript_DSA/getGrade.js
@@ -6,3 +6,7 @@ function getGrade(score) {
     return grade;
 }
 //Test cases
+console.log(getGrade(17)); // Expected output: 'C'
+console.log(getGrade(28)); // Expected output: 'A'
+console.log(getGrade(10)); // Expected output: 'F'
+console.log(getGrade(3));  // Expected output: 'F'
