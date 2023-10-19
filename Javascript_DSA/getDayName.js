f198029eccb35d2f7ad165fd1061e6261b257261
jugosack <jugoslav_ackoski@yahoo.com>
2023-10-19 23:01:00 +0200

    Add getDayName.js solution

--- Javascript_DSA/getDayName.js
+++ Javascript_DSA/getDayName.js
@@ -1,6 +1,13 @@
 function getDayName(dateString) {
     let dayName;
+    const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    
+    const date = new Date(dateString);
+    const dayIndex = date.getUTCDay();
+    
+    dayName = daysOfWeek[dayIndex];
     
     return dayName;
 }
 //Test cases
+console.log(getDayName(10/11/2009)); // Expected output: 'C'
