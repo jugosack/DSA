61191096a16be0f9b8179f0f9ff7728932920c8a
jugosack <jugoslav_ackoski@yahoo.com>
2023-10-21 16:01:07 +0300

    Add getMaxLessThanK.js solution

--- Javascript_DSA/getMaxLessThanK.js
+++ Javascript_DSA/getMaxLessThanK.js
@@ -1,6 +1,11 @@
 function getMaxLessThanK(n, k) {
     let max=0;
-    
+    for(let a = 1; a <= n; a++)
+        for(let b = a + 1; b <= n; b++) {
+            let x = a & b;
+            if (max < x &&  x < k)
+                    max = x;
+        }
         return max;
 }
 //Test case
