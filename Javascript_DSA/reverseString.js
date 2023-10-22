838ddd686dd35a4346b5b39089b481cb1fd59aa2
jugosack <jugoslav_ackoski@yahoo.com>
2023-10-22 22:23:47 +0300

    Add reverseString.js test case

--- Javascript_DSA/reverseString.js
+++ Javascript_DSA/reverseString.js
@@ -1,13 +1,28 @@
+// function reverseString(s) {
+//     const arr=[]
+//   const niza=[]
+//    for( const char of s){
+//     arr.push(char)
+//    }
+  
+//    for (let i = arr.length-1; i >= 0 ;i-- ) {
+//     niza.push(arr[i])
+//    }
+//    return niza.join("");
+// }
+// // //Test cases
+// const myString = "World!";
+// const resultArray = reverseString(myString);
+// console.log(resultArray);
 function reverseString(s) {
-    const arr=[]
-
-   for( const char of s){
-    arr.push(char)
-   }
-
-   return arr;
-    
+    try{
+        console.log( s.split("").reverse().join(""));
+    }catch(err){
+        console.log("s.split is not a function");
+        console.log(s);
+    }
 }
-//Test cases
-console.log("Hello");
-console.log("World");
+// // //Test cases
+const myString = "World!";
+const resultArray = reverseString(myString);
+console.log(resultArray);
