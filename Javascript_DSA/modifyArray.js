ca3acfcbb6a8cb95d1a1e8d7e12f63fcdc0f8002
jugosack <jugoslav_ackoski@yahoo.com>
2023-10-24 23:42:22 +0200

    Add modifyArray.js test

--- Javascript_DSA/modifyArray.js
+++ Javascript_DSA/modifyArray.js
@@ -12,5 +12,5 @@ function modifyArray(nums) {
     return arr;
 }
 //Test cases
-console.log(modifyArray(1,2,3,4,5,6,7));
-console.log(modifyArray(5,4,3,6,8,9));
+console.log(modifyArray([1,2,3,4,5,6,7]));
+console.log(modifyArray([5,4,3,6,8,9]));
