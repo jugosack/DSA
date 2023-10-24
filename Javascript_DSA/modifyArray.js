23f8451d8ffdbc294f3e26e8c74bf9ac605f40f1
jugosack <jugoslav_ackoski@yahoo.com>
2023-10-24 23:35:09 +0200

    Add modifyArray.js solution

--- Javascript_DSA/modifyArray.js
+++ Javascript_DSA/modifyArray.js
@@ -1,5 +1,15 @@
-function modifyArray(nums){
-
+function modifyArray(nums) {
+    let arr=[];
+    for (let i=0;i<nums.length; i++){
+        if (nums[i]%2==0){
+            let z=nums[i]*2;
+            arr.push(z)
+        }else{
+            let z=nums[i]*3;
+            arr.push(z)
+        }
+    }
+    return arr;
 }
 //Test cases
 console.log(modifyArray(1,2,3,4,5,6,7));
