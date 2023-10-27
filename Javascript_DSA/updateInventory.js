fa0e5c8c8c21db42bc234cd13c8d6ef8fda4280d
jugosack <jugoslav_ackoski@yahoo.com>
2023-10-27 12:46:33 +0200

    Add updateInventory.js strucuture

--- Javascript_DSA/updateInventory.js
+++ Javascript_DSA/updateInventory.js
@@ -1,23 +1,43 @@
 function updateInventory(arr1, arr2) {
+    for (let i = 0; i < arr2.length; i++) {
+      let found = false;
+  
+      for (let j = 0; j < arr1.length; j++) {
+        if (arr2[i][1] === arr1[j][1]) { // Compare item names
+          arr1[j][0] += arr2[i][0]; // Update the quantity
+          found = true;
+          break;
+        }
+      }
+  
+      if (!found) {
+        arr1.push(arr2[i]); // If the item is not found, add it to the inventory
+      }
+    }
+  
+    // Sort the inventory by item name
+    arr1.sort((a, b) => a[1].localeCompare(b[1]));
+  
     return arr1;
-}
-
-// Example inventory lists
-var curInv = [
+  }
+  
+  // Example inventory lists
+  var curInv = [
     [21, "Bowling Ball"],
     [2, "Dirty Sock"],
     [1, "Hair Pin"],
     [5, "Microphone"]
-];
-
-var newInv = [
+  ];
+  
+  var newInv = [
     [2, "Hair Pin"],
     [3, "Half-Eaten Apple"],
     [67, "Bowling Ball"],
     [7, "Toothpaste"]
-];
+  ];
+  
+  updateInventory(curInv, newInv);
 
-updateInventory(curInv, newInv);
 
 // EXPLANATION
 //Compare and update the inventory stored in a 2D array against a second 2D array of a fresh delivery. Update the current existing inventory item quantities (in arr1). 
