d702f46f96ec28c099ac7d6ebd9bb1eaa4e036ce
jugosack <jugoslav_ackoski@yahoo.com>
2023-10-31 09:49:01 +0100

    Add removeInvalidParentheses.js test cases

--- Javascript_DSA/removeInvalidParentheses.js
+++ Javascript_DSA/removeInvalidParentheses.js
@@ -2,3 +2,17 @@ var removeInvalidParentheses = function(s) {
 
 }
 //Test cases
+console.log(removeInvalidParentheses("()())()"));
+// Expected output: ["(())()", "()()()"]
+
+console.log(removeInvalidParentheses("(a)())()"));
+// Expected output: ["(a())()", "(a)()()"]
+
+console.log(removeInvalidParentheses(")("));
+// Expected output: [""]
+
+console.log(removeInvalidParentheses("()"));
+// Expected output: ["()"]
+
+console.log(removeInvalidParentheses("()("));
+// Expected output: ["()"]
