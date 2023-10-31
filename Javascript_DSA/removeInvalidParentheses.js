1d65e863710f6da5c05ef02ea0046f595c2df5ec
jugosack <jugoslav_ackoski@yahoo.com>
2023-10-31 09:53:33 +0100

    Add removeInvalidParentheses.js solution

--- Javascript_DSA/removeInvalidParentheses.js
+++ Javascript_DSA/removeInvalidParentheses.js
@@ -1,6 +1,75 @@
 var removeInvalidParentheses = function(s) {
+    const queue = [s];
+    const visited = new Set();
+    const result = [];
 
+    while (queue.length > 0) {
+        const current = queue.shift();
+
+        if (isValid(current)) {
+            result.push(current);
+            // You can continue searching for more valid expressions
+        }
+
+        if (result.length === 0) {
+            // If no valid expressions found yet, generate new expressions
+            for (let i = 0; i < current.length; i++) {
+                if (current[i] !== '(' && current[i] !== ')') continue;
+                const next = current.slice(0, i) + current.slice(i + 1);
+                if (!visited.has(next)) {
+                    queue.push(next);
+                    visited.add(next);
+                }
+            }
+        }
+    }
+
+    return result;
+};
+
+function isValid(s) {
+    let count = 0;
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] === '(') {
+            count++;
+        } else if (s[i] === ')') {
+            count--;
+        }
+        if (count < 0) return false;
+    }
+    return count === 0;
 }
+
+console.log(removeInvalidParentheses("()())()"));
+// Expected output: ["(())()", "()()()"]
+
+console.log(removeInvalidParentheses("(a)())()"));
+// Expected output: ["(a())()", "(a)()()"]
+
+console.log(removeInvalidParentheses(")("));
+// Expected output: [""]
+
+console.log(removeInvalidParentheses("()"));
+// Expected output: ["()"]
+
+console.log(removeInvalidParentheses("()("));
+// Expected output: ["()"]
+
+console.log(removeInvalidParentheses("((()"));
+// Expected output: ["()"]
+
+console.log(removeInvalidParentheses(")))"));
+// Expected output: [""]
+
+console.log(removeInvalidParentheses("()())()(()("));
+// Expected output: ["(())()()", "(()())()"]
+
+console.log(removeInvalidParentheses("()())(a)(()("));
+// Expected output: ["(())(a)()", "()()()"]
+
+console.log(removeInvalidParentheses("((()())(a(()"));
+// Expected output: ["(())(a())", "(())(a)()", "(()())(a())", "(()())(a)"]
+
 //Test cases
 console.log(removeInvalidParentheses("()())()"));
 // Expected output: ["(())()", "()()()"]
