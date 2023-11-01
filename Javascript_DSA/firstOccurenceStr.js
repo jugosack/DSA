c81a1b12367c63f39a3bc8a87869f1a03d07e2fe
jugosack <jugoslav_ackoski@yahoo.com>
2023-11-01 20:43:47 +0100

    Add firstOccurenceStr.js link

--- Javascript_DSA/firstOccurenceStr.js
+++ Javascript_DSA/firstOccurenceStr.js
@@ -5,3 +5,5 @@ var strStr = function(haystack, needle) {
 //Test case
 console.log(strStr("sadbutsad", "sad")); // Output: 0
 console.log(strStr("leetcode", "leeto")); // Output: -1
+
+//https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string/
