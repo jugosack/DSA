6465d4647e54e3cb7f34b8fef1aabdbb4247e3cd
jugosack <jugoslav_ackoski@yahoo.com>
2023-11-01 20:43:12 +0100

    Add firstOccurenceStr.js test cases

--- Javascript_DSA/firstOccurenceStr.js
+++ Javascript_DSA/firstOccurenceStr.js
@@ -3,5 +3,5 @@ var strStr = function(haystack, needle) {
 };
 
 //Test case
-console.log(strStr(sadbutsad,sad));
-console.log(strStr(leetcode,leeto));
+console.log(strStr("sadbutsad", "sad")); // Output: 0
+console.log(strStr("leetcode", "leeto")); // Output: -1
