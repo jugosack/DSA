e71a52326e0948fea99e4dcc916325cff0628f9c
jugosack <jugoslav_ackoski@yahoo.com>
2023-11-02 22:06:00 +0100

    Add firstOccurenceStr.js solution

--- Javascript_DSA/firstOccurenceStr.js
+++ Javascript_DSA/firstOccurenceStr.js
@@ -2,6 +2,11 @@ var strStr = function(haystack, needle) {
     if (needle === "") {
         return 0;
     }
+    for (let i = 0; i < haystack.length; i++) {
+        if (haystack.substring(i, i + needle.length) === needle) {
+            return i;
+        }
+    }
 };
 
 //Test case
