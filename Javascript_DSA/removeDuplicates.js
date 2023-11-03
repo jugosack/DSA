8b7be69ffd3045e3c09bdc4952b4a6a68a5364b9
jugosack <jugoslav_ackoski@yahoo.com>
2023-11-03 22:43:05 +0100

    Add removeDuplicates.js link

--- Javascript_DSA/removeDuplicates.js
+++ Javascript_DSA/removeDuplicates.js
@@ -3,3 +3,6 @@ var removeDuplicates = function(nums) {
 };
 //Test cases
 console.log(removeDuplicates([0,0,1,1,1,2,2,3,3,4])); // Expected output: 1
+
+
+//link: https://leetcode.com/problems/remove-duplicates-from-sorted-array/
