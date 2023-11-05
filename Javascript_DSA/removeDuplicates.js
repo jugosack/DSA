f277f01c7bb358ab7ebcd2606370bb75b86be598
jugosack <jugoslav_ackoski@yahoo.com>
2023-11-05 15:20:20 +0100

    Add removeDuplicates.js decision

--- Javascript_DSA/removeDuplicates.js
+++ Javascript_DSA/removeDuplicates.js
@@ -5,7 +5,7 @@ var removeDuplicates = function(nums) {
     uniqueIndex=0;
     for(let i=1; i<nums.length; i++){
         if(nums[i] != nums[uniqueIndex]){
-
+            uniqueIndex++;
         }
     }
 };
