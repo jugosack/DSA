a8a5d9d40cca16f9edb2d5e8b06e3a5d8fcdaa31
jugosack <jugoslav_ackoski@yahoo.com>
2023-11-05 15:21:59 +0100

    Add removeDuplicates.js decision

--- Javascript_DSA/removeDuplicates.js
+++ Javascript_DSA/removeDuplicates.js
@@ -10,7 +10,7 @@ var removeDuplicates = function(nums) {
         }
     }
 
-    return uniqueIndex;
+    return uniqueIndex +1;
 };
 //Test cases
 console.log(removeDuplicates([0,0,1,1,1,2,2,3,3,4])); // Expected output: 1
