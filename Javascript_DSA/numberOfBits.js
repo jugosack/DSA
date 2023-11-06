24e52d78bfe8f51e56731bb236ac20ad7b8d606e
jugosack <jugoslav_ackoski@yahoo.com>
2023-11-06 23:37:15 +0100

    Add numberOfBits.js link

--- Javascript_DSA/numberOfBits.js
+++ Javascript_DSA/numberOfBits.js
@@ -4,3 +4,5 @@ var hammingWeight = function(n) {
 //Test cases
 console.log(hammingWeight(00000000000000000000000000001011)); //Excpected: 3
 console.log(hammingWeight(00000000000000000000000010000000)); //Excpected: 1
+
+//link: https://leetcode.com/problems/number-of-1-bits/description/
