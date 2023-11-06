ddc9d6ef32579a878dbdea31dbddc7d8a1a1c781
jugosack <jugoslav_ackoski@yahoo.com>
2023-11-06 23:36:14 +0100

    Add numberOfBits.js test cases

--- Javascript_DSA/numberOfBits.js
+++ Javascript_DSA/numberOfBits.js
@@ -3,3 +3,4 @@ var hammingWeight = function(n) {
 };
 //Test cases
 console.log(hammingWeight(00000000000000000000000000001011)); //Excpected: 3
+console.log(hammingWeight(00000000000000000000000010000000)); //Excpected: 1
