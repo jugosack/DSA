1fccf393ff5b1a9ebecf205a7b8655be87ae9f80
jugosack <jugoslav_ackoski@yahoo.com>
2023-11-09 23:33:57 +0100

    Add isHappy.js

--- Javascript_DSA/isHappy.js
+++ Javascript_DSA/isHappy.js
@@ -13,7 +13,12 @@
 
 function sumOfSquares(num) {
     let sum = 0;
-  
+    while (num > 0) {
+        const digit = num % 10;
+        sum += digit * digit;
+        num = Math.floor(num / 10);
+    }
+    return sum;
 }
 //Test cases
 console.log(isHappy(2));
