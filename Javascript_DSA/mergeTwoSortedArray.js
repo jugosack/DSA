8e2b2a3f4da9d7a8841e4aab235d57eaaf7e19c7
jugosack <jugoslav_ackoski@yahoo.com>
2023-11-10 17:59:34 +0100

    Add mergeTwoSortedArray.js test cases

--- Javascript_DSA/mergeTwoSortedArray.js
+++ Javascript_DSA/mergeTwoSortedArray.js
@@ -8,3 +8,12 @@
  var merge = function(nums1, m, nums2, n) {
  };
  //test cases
+ // Example 1
+let nums1 = [1,2,3,0,0,0];
+let m = 3;
+let nums2 = [2,5,6];
+let n = 3;
+merge(nums1, m, nums2, n);
+console.log(nums1); // Output: [1,2,2,3,5,6]
+
+
