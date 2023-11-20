5469a0c41ac27ccaf2a45d7e3019b390c948b592
jugosack <jugoslav_ackoski@yahoo.com>
2023-11-20 23:37:28 +0100

    Add majorityElement.js link

--- Javascript_DSA/majorityElement.js
+++ Javascript_DSA/majorityElement.js
@@ -9,3 +9,5 @@
 //test cases
 console.log(majorityElement([3, 3, 4, 2, 4, 4, 2, 4, 4])); // Example test case
 console.log(majorityElement([2, 2, 1, 1, 1, 2, 2])); // Example test case
+
+//link: https://leetcode.com/problems/majority-element/
