d33d70666b0e85807eeab60aaf5ddc0a1d7dc691
jugosack <jugoslav_ackoski@yahoo.com>
2023-11-21 15:29:00 +0100

    Add majorityElement.js solution

--- Javascript_DSA/majorityElement.js
+++ Javascript_DSA/majorityElement.js
@@ -5,6 +5,13 @@
  var majorityElement = function(nums) {
     let candidate = null;
   let count = 0;
+  for (let num of nums) {
+    if (count === 0) {
+      candidate = num;
+    }
+
+    count += num === candidate ? 1 : -1;
+  }
     
 };
 
