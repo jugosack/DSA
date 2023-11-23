7e707e140f768b98bbd8e2963708578a58cacbc0
jugosack <jugoslav_ackoski@yahoo.com>
2023-11-23 21:02:52 +0100

    Add SearchInsertPosition.js solution

--- Javascript_DSA/SearchInsertPosition.js
+++ Javascript_DSA/SearchInsertPosition.js
@@ -5,7 +5,11 @@
  */
  var searchInsert = function(nums, target) {
     for (let i = 0; i < nums.length; i++) {
-       
+        if (nums[i] === target) {
+            return i;
+        }
+
+      
     }
 
     return nums.length;
